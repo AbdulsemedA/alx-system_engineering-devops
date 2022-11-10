c35da48fa35471f0746d3d3b42e2f0c48d229109
Abdulsemed Abdelila Andya <88716122+AbdulsemedA@users.noreply.github.com>
2022-11-10 10:17:54 +0300

    Update 1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,6 +1,13 @@
-# Fix for nginx
-exec { 'increase file limit':
-command  => 'awk \' $1 == "holberton" { $4=50000 }1\' /etc/security/limits.conf > tmp && mv tmp /etc/security/limit.conf',
-path     => ['/usr/bin', '/bin', '/usr/sbin'],
-provider => 'shell'
+# Enable the user holberton to login and open files without error.
+
+# Increase hard file limit for Holberton user.
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+# Increase soft file limit for Holberton user.
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
 }
